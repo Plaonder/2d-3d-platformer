bf6303f87b10525ba56afa442381f254c14b7dc7
Plaonder <39774970+Plaonder@users.noreply.github.com>
2021-07-16 07:15:32 -0700

    Add files via upload

---
+++ Scripts/GameManager.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager
+{
+    public static KeyCode switchKey { get; set; }
+    public static bool is3D;
+    public static LayerMask groundMask;
+
+    public static string playerTag;
+    public static string cubeTag;
+}
---
+++ Scripts/TriggerColliding.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerColliding : MonoBehaviour
+{
+    [Header("Trigger Collider")]
+    public Collider triggerCollider;
+
+    public bool isTriggerColliding;
+
+    private void Start()
+    {
+        triggerCollider = GetComponent<Collider>();
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        print("I am inside of a trigger");
+        if (other.gameObject.CompareTag(GameManager.playerTag) || other.gameObject.CompareTag(GameManager.cubeTag))
+        {
+            isTriggerColliding = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        print("I am exiting a trigger");
+        if (other.gameObject.CompareTag(GameManager.playerTag) || other.gameObject.CompareTag(GameManager.cubeTag))
+        {
+            isTriggerColliding = false;
+        }
+    }
+}
